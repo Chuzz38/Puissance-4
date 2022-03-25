f2af587e4739b1d71e9c737c97dbc764b881c65d
Chuzz38 <102356235+Chuzz38@users.noreply.github.com>
2022-03-25 11:36:30 +0100

    Update p4_game.py

--- puissance 4/modules/p4_game.py
+++ puissance 4/modules/p4_game.py
@@ -1,1 +1,28 @@
-.
+."""On va créer une classe puissance 4 qui aura les différents attributs
+et les différentes méthodes pour rendre le puissance 4 possible"""
+
+
+""". ; O ; X --> interface, affichage
+0 : case vide ; 1 : case du joueur un ; 2 : case du jouer 2
+pour pouvoir stocker au fur et mesure du jeu"""
+
+"""tester à chaque fois si il y a victoire et si c'est vide on ne peut pas mettre au dessus : retourne si """
+
+class P4_game:
+    plateau = 
+    tourJoueur = 
+
+    def jeu_possible():
+        pass
+
+
+    def get_case(x,y):
+        pass
+
+
+    def victoire():
+        pass
+
+
+    def jeu(joueur, tab):
+        pass
