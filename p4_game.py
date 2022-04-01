a07c80008dcf2db697d28b640b1e323b14ebaefe
Chuzz38 <102356235+Chuzz38@users.noreply.github.com>
2022-04-01 10:19:44 +0200

    Update p4_game.py

--- p4_game.py
+++ p4_game.py
@@ -3,76 +3,51 @@ import random
 et les différentes méthodes pour rendre le puissance 4 possible"""
 
 
-""". ; O ; X --> interface, affichage
-0 : case vide ; 1 : case du joueur un ; 2 : case du jouer 2
+"""
+0 : case vide ; 1 : case du joueur 1 ; 2 : case du joueur 2
 pour pouvoir stocker au fur et mesure du jeu"""
 
 """tester à chaque fois si il y a victoire et si c'est vide on ne peut pas mettre au dessus : retourne si """
 
 class P4_game:
-    
-    def __init__(self, nl=6, nc=7,jeton=('O','X')):
+    def __init__(self, nl=6, nc=7,jeton=('1','2')):
         self.nl = nl
         self.nc = nc
         self.jeton = jeton
-        self.tableJeu = [['.' for i in range(nc)] for j in range(nl)]
+        self.tableJeu = [['0' for i in range(nc)] for j in range(nl)]
         self.nouvellepartie(self.nl, self.nc, self.jeton)
-
-    def tourJoueur(self, nbTour = 0):
-        x = 0
-        if self.nbTour == 0: #si la partie commence
-            x = random.randint(0, 1)
-            if x == 0:
-                return joueur1
-            else:
-                return joueur2
-        else:plus de '.' dans la ligne, elle est donc remplieueur1
-            else:
-                return joueur2
 
     def get_case(self, ligne, colonne):
         return self.tableJeu[ligne][colonne]
 
-
-    """def jeu_possible(self, ligne, colonne):
-        if self.get_case(self.ligne, self.colonne) == '.':
-            return True
-        else: #si la case est déja prise (O ou X)
-            return False"""
-
     def placement_possible(self, colonne):
         """Prend en parametre seulement la colonne et renvoie 
         True si la case est libre et False sinon"""
-        for i in range(ligne): #?
-            if self.get_case(,)
+        return self.tableJeu[0][colonne] == 0 
 
     def placer_jeton(self, joueur, colonne):
         """ Joue le coup du joueur dans la colonne choisie.
-        Retourne (False) si le coup n'est pas valide (si la colonne
-        est déjà remplie)
-        Retourne (True, ligne) si le coup est valide, avec ligne la ligne dans
-        laquelle se trouve le jeton jouée. Si le coup est valide,
-        la méthode modifie la table de jeu en cours.
+        Si la colonne n'est pas disponible il faut choir une autre colonne
         """
-        if placement_possible(colonne, joueur): # == True ?
-    
+        if not placement_possible(colonne, joueur): 
+            print("Vous ne pouvez pas jouer dans cette colonne")
+        else:
+            self.tableJeu[self.ligne][colonne] == self.jeton[joueur-1]
 
-    def afficher_table(self, joueur, tableJeu):
-        pass #montrer la table de jeu à un moment donné
-
-
-    def victoire(self, ligne, colonne, joueur):
+    def verifier_victoire(self, ligne, colonne, joueur):
         """ vérifie si le dernier coup joué, qui a placé le jeton
         de joueur en (ligne,colonne) permet de gagner, on devra 
         tester toutes les combinaisons possibles : colonne, ligne
         et diagonales
         """
-        gagné = self.jeton[joueur-1] * 4 #on donne la combinaison pour que ça soit gagné
-        for i in range(ligne):
-            for j in range(colonne):
+        victoire = False
+        victoirePossible = self.jeton[joueur-1] * 4 #on donne la combinaison pour que ça soit gagné
+        #vérifier si la ligne est correcte
+        ligne_tableJeu = [ligne[colonne]for colonne in self.tableJeu]
+        while not victoire:
+            
 
-
-    def nouvellepartie(self, nl=6, nc=7, jeton =('O', 'X'))
+    def nouvellepartie(self, nl=6, nc=7, jeton =('1', '2'))
         pass
 
 if __name__ == "__main__":
