6a212b74960e84520cecf477cf577306564dcae4
AyKouuZ <rebelo.alexis@gmail.com>
2022-04-06 10:54:14 +0200

    a

---
+++ p4_console.py
@@ -0,0 +1,54 @@
+from p4_game import *
+from IPG4
+
+class P4_console:
+    def __init__(self, nl=6, nc=7,jeton=('1','2'), joueur=(1,2)):
+        self.nl = nl
+        self.nc = nc
+        self.jeton = jeton
+        self.joueur = joueur 
+        self.tableJeu = [['0' for i in range(nc)] for j in range(nl)]
+        dd
+
+    def demande():
+        joueur1 = input("Ecrivez le nom du joueur 1 : ")
+        joueur2 = input("Ecrivez le nom du joueur 2 : ")
+        print(" ")
+
+    def afficheTableau(self):
+        for l in range (nl):
+            for c in range (nc):
+                print(tableau[l][c], end = " ")
+            print()
+
+    def verifieLignes():
+        pass
+
+
+    def ajoutePion(colonne, pion):
+        for i in range (nl-1, -1, -1):
+            if tableau[i][colonne]==".":
+                tableau[i][colonne] = pion
+                print("placee en ligne",i)
+                break
+
+
+    while not P4_game.verifier_victoire(ligne, colonne, joueur):
+        afficheTableau()
+        print(" ")
+        print(joueur1,"c'est ton tour")
+        print(" ")
+        if not placement_possible():
+            print("Vous devez rejouer")
+        else:
+            ajoutePion(affichage_pion(joueur, '@')
+        
+
+        afficheTableau()
+        print(" ")
+        print(joueur2,"c'est ton tour")
+        print(" ")
+        if not placement_possible():
+            print("Vous devez rejouer")
+        else:
+            ajoutePion(affichage_pion(joueur), 'X')
