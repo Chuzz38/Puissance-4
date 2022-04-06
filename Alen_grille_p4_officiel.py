7dd85dfaa05430b9066d00a69c425b5c8dbe06e5
AyKouuZ <rebelo.alexis@gmail.com>
2022-04-06 10:40:38 +0200

    aa

--- Alen_grille_p4_officiel.py
+++ Alen_grille_p4_officiel.py
@@ -1,20 +1,21 @@
 from p4_game import *
 
 class P4_console:
-    joueur1 = input("Ecrivez le nom du joueur 1 : ")
-    joueur2 = input("Ecrivez le nom du joueur 2 : ")
-    print(" ")
+    def __init__(self, nl=6, nc=7,jeton=('1','2'), joueur=(1,2)):
+        self.nl = nl
+        self.nc = nc
+        self.jeton = jeton
+        self.joueur = joueur 
+        self.tableJeu = [['0' for i in range(nc)] for j in range(nl)]
 
-
-    colonne = 7
-    ligne = 6
-
-
-    tableau = [['.','.','.','.','.','.','.'],['.','.','.','.','.','.','.'],['.','.','.','.','.','.','.'],['.','.','.','.','.','.','.'],['.','.','.','.','.','.','.'],['.','.','.','.','.','.','.']]
+    def demande():
+        joueur1 = input("Ecrivez le nom du joueur 1 : ")
+        joueur2 = input("Ecrivez le nom du joueur 2 : ")
+        print(" ")
 
     def afficheTableau(self):
-        for l in range (ligne):
-            for c in range (colonne):
+        for l in range (nl):
+            for c in range (nc):
                 print(tableau[l][c], end = " ")
             print()
 
@@ -23,7 +24,7 @@ class P4_console:
 
 
     def ajoutePion(colonne, pion):
-        for i in range (LIGNE-1, -1, -1):
+        for i in range (nl-1, -1, -1):
             if tableau[i][colonne]==".":
                 tableau[i][colonne] = pion
                 print("placee en ligne",i)
